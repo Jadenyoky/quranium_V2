70c3739bda1ea6ff8cbabd8ce81008e2fcd8e726
jadenyoky <jaden_yoky@yahoo.com>
2024-08-29 14:25:08 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
             type: "image/png",
           },
         ],
-        orientation: "portrait",
+        orientation: "portrait-primary",
         display: "fullscreen",
         name: "Quranium",
         short_name: "Quranium",
